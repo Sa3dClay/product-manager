10f83cb8d1d331fe46b82175586c477187394dab
Abdulrahman Atef <sa3d.clay@gmail.com>
2023-03-18 20:21:50 +0200

    return product image full path

--- app/Http/Resources/ProductImageResource.php
+++ app/Http/Resources/ProductImageResource.php
@@ -17,7 +17,7 @@ class ProductImageResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'image_path' => Storage::url($this->image_path),
+            'image_path' => env('APP_URL') . Storage::url($this->image_path),
         ];
     }
 }
